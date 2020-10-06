ccfb11705cb4d6fafa24bfed1293f6c77ff6af50
Amit Mangal <mangalamitash@gmail.com>
2020-10-07 00:40:12 +0530

    Android build working

--- android/app/src/main/java/com/fridayapp/MainApplication.java
+++ android/app/src/main/java/com/fridayapp/MainApplication.java
@@ -1,12 +1,20 @@
 package com.fridayapp;
 
 
+import android.content.Context;
+
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.soloader.SoLoader;
+import java.lang.reflect.InvocationTargetException;
+
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
-import com.reactnativenavigation.react.ReactGateway;
+//import com.reactnativenavigation.react.ReactGateway;
+
+import com.facebook.react.PackageList;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +30,7 @@ public class MainApplication extends NavigationApplication {
             @Override
             protected List<ReactPackage> getPackages() {
                 @SuppressWarnings("UnnecessaryLocalVariable")
-                List<ReactPackage> packages = getPackages();
+                List<ReactPackage> packages = new PackageList(this).getPackages();
                 // Packages that cannot be autolinked yet can be added manually here, for example:
                 // packages.add(new MyReactNativePackage());
                 return packages;
@@ -42,6 +50,44 @@ public class MainApplication extends NavigationApplication {
         return Arrays.<ReactPackage>asList(
                 // eg. new VectorIconsPackage()
         );
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // SoLoader.init(this, /* native exopackage */ false);
+        initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    }
+
+    /**
+     * Loads Flipper in React Native templates. Call this in the onCreate method with something like
+     * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+     *
+     * @param context
+     * @param reactInstanceManager
+     */
+    private static void initializeFlipper(
+            Context context, ReactInstanceManager reactInstanceManager) {
+    if (BuildConfig.DEBUG) {
+        try {
+            /*
+                We use reflection here to pick up the class that initializes Flipper,
+            since Flipper library is not available in release mode
+            */
+            Class<?> aClass = Class.forName("com.fridayapp.ReactNativeFlipper");
+            aClass
+                .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+                .invoke(null, context, reactInstanceManager);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 //    @Override
