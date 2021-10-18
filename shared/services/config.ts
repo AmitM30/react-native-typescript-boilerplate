f81aeeea698503da98cb431873c278c462c3a2ac
Amit Mangal <amangal@vrbo.com>
2021-10-18 12:40:24 +0530

    remove key

--- shared/services/config.ts
+++ shared/services/config.ts
@@ -1,8 +1,7 @@
 export default {
   APIs: {
     host: 'amazon-data-scraper15.p.rapidapi.com',
-    token: '9060639b0cmsh3e48cc5ea6c1685p11bebejsn5ce7c058be49',
-    api_key: '55d4588fcd6ce77b428b73d97f714195',
+    token: '',
     endpoints: {
       baseAPI: 'https://amazon-data-scraper15.p.rapidapi.com/',
       search: 'search/'
